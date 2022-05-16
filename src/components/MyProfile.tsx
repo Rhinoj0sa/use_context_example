1538aad61802d909e67ada057a61b074490cc42a
Roberto Hinojosa <rhinojosaram@gmail.com>
2022-05-16 11:43:30 -0500

    Minor changes

--- src/components/MyProfile.tsx
+++ src/components/MyProfile.tsx
@@ -1,12 +1,13 @@
 import { useGlobalContext } from "./MyGlobalContext";
 
 const MyProfile = () => {
-  const { copy } = useGlobalContext();
+  const { copy, risk } = useGlobalContext();
   return (
     <div>
       <hr />
-      <h2> Profile</h2>
-      <h3>{copy}</h3>
+      <h2> Profile component </h2>
+      <h3>Copy: {copy}</h3>
+      <h4>Risk: {risk}</h4>
     </div>
   );
 };
